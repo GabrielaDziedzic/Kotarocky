571128b10d114a087931aa9eb250ab828b24f100
creat73 <nikill1125@gmail.com>
2017-09-10 17:48:27 +0200

    Create kwiaty_dla_kota function.

--- gittie.py
+++ gittie.py
@@ -1,2 +1,11 @@
 def introduce():
     print("Hello, I'm Gittie!")
+
+def kwiaty_dla_kota():
+    kwiaty = """
+    <>  <> <>
+     \  |  /
+      \ | /
+       \ /"""
+    print(kwiaty)
+    print("antyalergiczne")
